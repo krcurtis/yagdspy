59ab26385d4c223fdcf1298f6f71434be5a29496
krcurtis <krcurtis@fhcrc.org>
2018-05-30 13:27:22 -0700

    first pass on adding support for planning

---
+++ yagdspy/planning.py
@@ -0,0 +1,31 @@
+# Copyright 2016, 2017 Fred Hutchinson Cancer Research Center
+################################################################################
+### Support planning operations
+
+
+import re
+from .processing_module import *
+
+
+
+def regex_ize(pathname_template):
+    """Return regex pattern to try to match to pathname template, that use curly braces for arguments"""
+    values = re.split("{[^}]+}", pathname_template)
+    return '\A' + '.+'.join(values) + '\Z'
+
+
+def suggest_module(out_file):
+    results = []
+    for name, inputs, outputs in processing_module_info:
+        for v in inputs.values():
+            pattern = regex_ize(v)
+            m = re.search(pattern, out_file)
+            if None != m:
+                results.append(name)
+        for v in outputs.values():
+            pattern = regex_ize(v)
+            m = re.search(pattern, out_file)
+            if None != m:
+                results.append(name)
+    return results
+
